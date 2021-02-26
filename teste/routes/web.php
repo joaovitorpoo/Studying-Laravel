5a149261434c4664c7bb36980fddd90e356ae4a8
João Vitor <63071327+joaovitorpoo@users.noreply.github.com>
2021-02-26 01:13:22 -0300

    Rotas com parametros

--- teste/routes/web.php
+++ teste/routes/web.php
@@ -24,3 +24,12 @@ Route::get('/teste', function () {
 Route::get('/ola/sejabemvindo', function () {
     echo 'Ola! Seja bem vindo.';
 });
+
+// Rotas com parametros 
+Route::get('/ola/{nome}', function ($nome) {
+    echo 'Ola! Seja bem vindo'. $nome;
+});
+
+Route::get('/ola/{nome}/{sobrenome}', function ($nome, $sobrenome) {
+    echo 'Ola! Seja bem vindo '. $nome . " " .$sobrenome;
+});
