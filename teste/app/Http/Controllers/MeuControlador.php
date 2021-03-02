c929042b2351729f734e7c5a7f09782116f423fa
João Vitor <63071327+joaovitorpoo@users.noreply.github.com>
2021-03-02 11:01:16 -0300

    Montando o controlador

--- teste/app/Http/Controllers/MeuControlador.php
+++ teste/app/Http/Controllers/MeuControlador.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class MeuControlador extends Controller
 {
-    //
+    public function produtos() {
+        echo "<h1>Produtos</h1>";
+        echo "<ol>";
+        echo "<li>Notebook</li>";
+        echo "<li>Impressora</li>";
+        echo "<li>Mouse</li>";
+        echo "</ol>";
+    }
+
+    public function getNome(){
+        return "João Vitor";
+    }
+
+    public function getIdade(){
+        return 20;
+    }
+
+    public function multiplicar($n1, $n2){
+        return $n1 * $n2;
+    }
 }
--- teste/routes/web.php
+++ teste/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
+use App\Http\Controllers\MeuControlador;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,88 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/teste', function () {
-    echo 'Ola! Seja bem vindo.';
-});
+// linkando rota com controlador
+Route::get('produtos', [MeuControlador::class, 'produtos']);
+Route::get('nome', [MeuControlador::class, 'getNome']);
+Route::get('idade', [MeuControlador::class, 'getIdade']);
 
-Route::get('/ola/sejabemvindo', function () {
-    echo 'Ola! Seja bem vindo.';
-});
-
-// Rotas com parametros 
-Route::get('/ola/{nome}', function ($nome) {
-    echo 'Ola! Seja bem vindo'. $nome;
-});
-
-Route::get('/ola/{nome}/{sobrenome}', function ($nome, $sobrenome) {
-    echo 'Ola! Seja bem vindo '. $nome . " " .$sobrenome;
-});
-
-// Rota com parametros opcionais
-Route::get('/seunome/{nome?}', function ($nome=null) {
-    if (isset($nome))
-        return 'Ola! Seja bem vindo, '. $nome.'!';
-    return 'Voce nao digitou o seu nome.';
-});
-
-// Rota com regras usando Expressão regular 
-Route::get('/rotacomregras/{nome}/{n}', function ($nome, $n) {
-    for ($i=0; $i < $n; $i++) 
-        echo 'Ola! Seja bem vindo, '. $nome .'! <br>';
-})->where('nome','[A-Za-z]+')->where('n','[0-9]+');
-
-// Agrupamento de rotas
-Route::prefix('/app')->group(function(){
-
-    Route::get('/', function () {
-        return view('app');
-    })->name('app'); // Nomeando rota
-
-    Route::get('/user', function () {
-        // Mandando para view
-        return view('user');
-    })->name('app.user');
-
-    Route::get('/profile', function () {
-        return view('profile');
-    })->name('app.profile');
-
-});
-
-Route::get('/produtos', function () {
-    echo "<h1>Produtos</h1>";
-    echo "<ol>";
-    echo "<li>Notebook</li>";
-    echo "<li>Impressora</li>";
-    echo "<li>Mouse</li>";
-    echo "</ol>";
-})->name('meusprodutos');
-
-// Redirecionamento requisições
-Route::redirect('/todosprodutos1', '/produtos', 301);
-
-Route::get('/todosprodutos2', function () {
-    return redirect()->route('meusprodutos');
-});
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-Route::post('/requisicoes', function (Request $request) {
-    return 'Hello POST';
-});
-
-Route::delete('/requisicoes', function (Request $request) {
-    return 'Hello DELETE';
-});
-
-Route::put('/requisicoes', function (Request $request) {
-    return 'Hello PUT';
-});
-
-Route::patch('/requisicoes', function (Request $request) {
-    return 'Hello PATCH';
-});
-
-Route::options('/requisicoes', function (Request $request) {
-    return 'Hello OPTIONS';
-});
+// passando parametros para funcao do controlador
+Route::get('multiplicar/{n1}/{n2}', [MeuControlador::class, 'multiplicar']);
