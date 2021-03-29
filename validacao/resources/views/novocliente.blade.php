54db5f1a7ebaa5bfebd30d7e0807702bb0cd8eac
João Vitor <63071327+joaovitorpoo@users.noreply.github.com>
2021-03-29 15:43:54 -0300

    Ambiente Projeto Validacao

---
+++ validacao/resources/views/novocliente.blade.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <title>Cadastro de Clientes</title>
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+    </head>
+<body>
+    <script src="{{ asset('js/app.js')}}" type="text/javascript"></script>
+</body>
+</html>
